2092c544869061bb37918bf602f13ba5f817cd8a
unknown <shazaly.se@gmail.com>
2021-05-21 00:15:46 -0700

    back end

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('/users', 'UserController@index');
+
+    
+});
